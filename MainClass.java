864251e468c3b7fbdd16da53467f9441e45c7521
anilnadgeri <anilnadgeri@yahoo.com>
2017-08-16 14:11:34 +0530

    updates from eclipse

--- MainClass.java
+++ MainClass.java
@@ -3,7 +3,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello !! welcome to Anil's repo last time”);
+		System.out.println("Hello !! updates from eclipse”);
 	}
 
 }
